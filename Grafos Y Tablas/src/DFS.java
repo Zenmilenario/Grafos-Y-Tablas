9f90b19bb93d447cf81dabb3c353d014d5737284
alp161093 <alvaroldp93@gmail.com>
2024-05-22 12:13:20 +0200

    cambios en busqueda en profundidad(DFS)

--- Grafos Y Tablas/src/DFS.java
+++ Grafos Y Tablas/src/DFS.java
@@ -1,21 +1,36 @@
 public class DFS {
-    private boolean[] visitados;
-
+    private int[] visitados;
+    private int[] noVisitados; 
+    
     public DFS(Grafo grafo, int inicio) {
-        visitados = new boolean[grafo.getVertices()];
-        System.out.print("DFS: ");
+        this.visitados = new int[grafo.getVertices()];
+        this.noVisitados = inicializarNoVisitados(grafo.getVertices());
+        
+        
+        System.out.print("Búsqueda en profundida/DFS: ");
         dfs(grafo.getMatrizAdyacencia(), inicio);
         System.out.println();
     }
 
     private void dfs(int[][] matrizAdyacencia, int v) {
-        visitados[v] = true;
+        visitados[v-1] = 1; //se añade el vértice a la lista de visitados
+        noVisitados[v-1] = 0; // se quita el vértice de la lista de no visitados
         System.out.print(v + " ");
 
         for (int i = 0; i < matrizAdyacencia[v].length; i++) {
-            if (matrizAdyacencia[v][i] != 0 && !visitados[i]) {
+            if (matrizAdyacencia[v][i] != 0 && visitados[i] == 0)
+            	//si tiene conexion y no esta visitado entonces lo analizamos
                 dfs(matrizAdyacencia, i);
-            }
         }
+    }
+    
+    //Se inicializan todos a uno al principio porque ninguno está visitado
+    public int[] inicializarNoVisitados(int vertices) {
+    	int[] noVisitados = new int[vertices];
+    	
+    	for (int i = 0; i < noVisitados.length; i++) {
+    		noVisitados[i] = 1;
+        }
+    	return noVisitados;
     }
 }
